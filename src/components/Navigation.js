21a2cabf84953f452f7cfdec3c9e4f2faa3acd5f
Marcelearn <jaworskimarcel@gmail.com>
2022-10-18 14:56:58 +0200

    second commit

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,9 @@
+import "./Navigation.css";
 import NavItem from "./NavItem";
 function Navigation() {
     return (
-        <nav>
-            <ul>
+        <nav className="Navigation">
+            <ul className="Navigation__List">
                 <NavItem navLabel="Link 1" navLink="https://google.de"/>
                 <NavItem navLabel="Link 2" navLink="https://google.de"/>
                 <NavItem navLabel="Link 3" navLink="https://google.de"/>
